48923aa9cc09d8cb16d57af1f43695848fd2469f
Krzysztof Mochocki <krzymoc340@student.polsl.pl>
2020-04-12 23:20:45 +0200

    Add project files.

---
+++ Andek/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+
+namespace Andek
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button copy_button;
+        private EditText input_pass;
+        private EditText output_pass;
+        private CheckBox small_letters_allowed;
+        private CheckBox capital_letters_allowed;
+        private CheckBox digits_allowed;
+        private CheckBox ambigous_characters_allowed;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // DO NOT TOUCH code
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            //Copy  Button
+            copy_button = FindViewById<Button>(Resource.Id.copy_button);
+            copy_button.Click += delegate { copy_output(); };
+
+            // Input Password
+            input_pass = FindViewById<EditText>(Resource.Id.input_password);
+            input_pass.TextChanged += delegate { sync(); };
+
+            // Output Password
+            output_pass = FindViewById<EditText>(Resource.Id.output_password);
+
+            // Checkboxes configuration
+            small_letters_allowed = FindViewById<CheckBox>(Resource.Id.small_letters_allowed);
+            small_letters_allowed.CheckedChange += delegate { sync(); };
+
+            capital_letters_allowed = FindViewById<CheckBox>(Resource.Id.capital_letters_allowed);
+            capital_letters_allowed.CheckedChange += delegate { sync(); };
+
+            digits_allowed = FindViewById<CheckBox>(Resource.Id.digits_allowed);
+            digits_allowed.CheckedChange += delegate { sync(); };
+
+            ambigous_characters_allowed = FindViewById<CheckBox>(Resource.Id.ambigous_characters_allowed);
+            ambigous_characters_allowed.CheckedChange += delegate { sync(); };
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void copy_output()
+        {
+            Clipboard.SetTextAsync(output_pass.Text);
+            Toast.MakeText(Android.App.Application.Context, "copied", ToastLength.Short).Show();
+        }
+        
+        void sync()
+        {
+            bool small_letters              = small_letters_allowed.Checked;
+            bool capital_letters            = capital_letters_allowed.Checked;
+            bool digits                     = digits_allowed.Checked;
+            bool ambigous_characters        = ambigous_characters_allowed.Checked;
+
+            if(!small_letters && !capital_letters && !digits && !ambigous_characters)
+            {
+                Toast.MakeText(Android.App.Application.Context, "No avaiable characters, resetting...", ToastLength.Short).Show();
+
+                small_letters_allowed.Checked = true;
+                capital_letters_allowed.Checked = true;
+                digits_allowed.Checked = true;
+                ambigous_characters_allowed.Checked = true;
+                sync();
+                return;
+            }
+
+            output_pass.Text = new PasswordEnchanter(input_pass.Text, small_letters, capital_letters, digits, ambigous_characters).generate_pass();
+        }
+
+
+    }
+}
---
+++ Andek/PasswordEnchanter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Andek
+{
+    class PasswordEnchanter
+    {
+        private struct pair { public char c1; public char c2; public pair(char a1, char a2) { c1 = a1; c2 = a2; } };
+        private pair[] translator = { new pair('1', '!'), new pair('a', '@'), new pair('3', 'E'), new pair('O', '0'), new pair('o', '0'), new pair('Q', 'O'), new pair('B', '8'), new pair('\'', '`'), new pair('l', '!'), new pair('S', '$') };
+
+        private string input;
+        private bool small_letters;
+        private bool capital_letters;
+        private bool digits;
+        private bool ambigous_characters;
+
+        public PasswordEnchanter( string _input, bool _small_letters, bool _capital_letters, bool _digits, bool _ambigous_characters)
+        {
+            input = _input;
+            small_letters = _small_letters;
+            capital_letters = _capital_letters;
+            digits = _digits;
+            ambigous_characters = _ambigous_characters;
+        }
+
+        public string generate_pass()
+        {
+
+            string output = input;
+
+            output = handle_whitespaces(output);
+            output = repair_capitalization(output);
+
+            output = capitalize(output);
+            output = ambigouise(output);
+
+            output = deambigouise(output);
+            output = dedigitalize(output);
+
+            return output;
+        }
+
+        string capitalize(string var)
+        {
+            if (!capital_letters) return var;
+
+            string data = "";
+            for (int i = 0; i < var.Length; i++)
+                if (new System.Random().Next(0, 100) < 50)
+                    data += char.ToUpper(var[i]);
+                else
+                    if (!small_letters)
+                    data += char.ToUpper(var[i]);
+                else
+                    data += var[i];
+            return data;
+        }
+
+        string handle_whitespaces(string var)
+        {
+            string data = "";
+            foreach (char x in var)
+                if (char.IsWhiteSpace(x)) { if (ambigous_characters) data += "_"; } else data += x;
+            return data;
+        }
+
+        string repair_capitalization(string var)
+        {
+            string data = "";
+            foreach (char x in var)
+            {
+                if (char.IsLetter(x))
+                {
+                    if (!small_letters && !capital_letters)
+                        data += repair(x);
+                    else if (!capital_letters)
+                        data += char.ToLower(x);
+                    else if (!small_letters)
+                        data += char.ToUpper(x);
+                    else data += x;
+                }
+                else data += x;
+            }
+            return data;
+        }
+
+        string dedigitalize(string var)
+        {
+            if (digits) return var;
+            string data = "";
+            for (int i = 0; i < var.Length; i++)
+                if (char.IsDigit(var[i])) data += repair(var[i]);
+                else data += var[i];
+            return data;
+        }
+
+        string ambigouise(string var)
+        {
+            if (!ambigous_characters) return var;
+            string data = "";
+            for (int i = 0; i < var.Length; i++)
+                if (new System.Random().Next(0, 100) < 75)
+                    data += get_alternative_character(var[i]);
+                else data += var[i];
+            return data;
+        }
+
+        string deambigouise(string var)
+        {
+            if (ambigous_characters) return var;
+            string data = "";
+            for (int i = 0; i < var.Length; i++)
+                if (!char.IsLetterOrDigit(var[i])) data += repair(var[i]);
+                else data += var[i];
+            return data;
+        }
+
+        char get_alternative_character(char c)
+        {
+            foreach (pair p in translator)
+                if (p.c1 == c)
+                    return p.c2;
+                else if (p.c2 == c)
+                    return p.c1;
+            return c;
+        }
+
+        char random_allowed_sign()
+        {
+            char[] ambi = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', ',', '.', '<', '>', '/', '?', '"', '\'', '\\', ';', ':', ']', '}', '{', '[' };
+            List<char> results = new List<char>();
+            if (small_letters) results.Add((char)new System.Random().Next(97, 123));
+            if (capital_letters) results.Add((char)new System.Random().Next(65, 91));
+            if (digits) results.Add((char)new System.Random().Next(48, 58));
+            if (ambigous_characters) results.Add((char)ambi[new System.Random().Next(0, ambi.Length)]);
+            return results[new System.Random().Next(0, results.Count)];
+        }
+
+        char repair(char c)
+        {
+            //first try to get alternative
+            char x = get_alternative_character(c);
+            if (x != c) return x;
+
+            //if no hard replace
+            return random_allowed_sign();
+        }
+    }
+}
